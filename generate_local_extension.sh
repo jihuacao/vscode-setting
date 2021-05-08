f7b43904f165a6fcfd7e4947ecbc0b86946389ca
caojihua <CJHdream@outlook.com>
2021-05-08 17:06:14 +0800

    update

---
+++ generate_local_extension.sh
@@ -0,0 +1,1 @@
+code --list-extensions --show-versions | xargs -L 1 echo code --install-extension  > extensions-local.sh & chmod +x extensions-local.sh
---
+++ generate_remote_extension.sh
@@ -0,0 +1,1 @@
+code --list-extensions --show-versions | xargs -L 1 echo code --install-extension  > extensions-remote.sh & chmod +x extensions-remote.sh
