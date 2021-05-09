9f3dd296562dc4bac514a0dcec27a9e28282ed80
”jihuacao“ <CJHdream@outlook.com>
2021-05-09 15:49:07 +0800

    update

--- generate_local_extension.sh
+++ generate_local_extension.sh
@@ -1,1 +1,1 @@
-code --list-extensions --show-versions | xargs -L 1 echo code --install-extension  > extensions-local.sh & chmod +x extensions-local.sh
+code --list-extensions --show-versions > extensions-local.sh & chmod +x extensions-local.sh
--- generate_remote_extension.sh
+++ generate_remote_extension.sh
@@ -1,1 +1,1 @@
-code --list-extensions --show-versions | xargs -L 1 echo code --install-extension  > extensions-remote.sh & chmod +x extensions-remote.sh
+code --list-extensions --show-versions | xargs -L 1 echo > extensions-remote.sh & chmod +x extensions-remote.sh
