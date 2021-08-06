3fa15c525f5ef13b056611287ced76d81d3a1954
”jihuacao“ <CJHdream@outlook.com>
2021-08-06 23:03:07 +0800

    update

--- install_extensions.sh
+++ install_extensions.sh
@@ -1,9 +1,9 @@
-if [ $0=="local" ]
+if [ $1 == "local" ]
 then
     # 获取每一行然后运行
     while read in; do code --install-extension $in; done < extensions-local.list
 else
-    if [ $0 == "remote" ]
+    if [ $1 == "remote" ]
     then
         while read in; do code --install-extension $in; done < extensions-remote.list
     fi
