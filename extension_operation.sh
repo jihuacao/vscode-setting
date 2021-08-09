a2a8f4de42e7f8383fd4bfe98206723e7068aa32
jihuacao <CJHdream@outlook.com>
2021-08-09 11:34:58 +0800

    record

--- extension_operation.sh
+++ extension_operation.sh
@@ -14,8 +14,7 @@ get_remote_supposed(){
 
 print_list(){
     arr=(${1})
-    for i in ${arr[*]}
-    do
+    for i in ${arr[*]};do
         echo ${i}
     done
 }
@@ -28,56 +27,81 @@ compare(){
     local two_own
     local found_in_two
 
-    for one in ${arr_1[*]}
-    do
+    for one in ${arr_1[*]};do
         found_in_two="notfound"
-        echo ${one}
+        #echo "one:${one}"
 
-        echo "arr_2 size:${#arr_2[@]}"
-        for ((two_index=0;two_index<${#arr_2[*]};two_index++))
+        #echo "arr_2 size:${#arr_2[@]}"
+        for ((two_index=0;two_index<${#arr_2[*]};two_index++));do
         #for two in ${arr_2[*]}
-        do
-            echo ${two_index}
+            #echo ${two_index}
             #echo ${arr_2[${two_index}]}
-            if [ ${one}=${arr_2[${two_index}]} ]
+            if [[ ${one} == "${arr_2[${two_index}]}" ]];then
             #echo ${two}
             #if [ ${one}=${two} ]
-            then
+                #echo "two:${arr_2[${two_index}]}"
                 unset arr_2[${two_index}]
+                arr_2=("${arr_2[@]}")
                 one_cap_two="${one_cap_two}|${one}"
                 found_in_two="found"
                 break
             fi
         done 
 
-        if [ ${found_in_two}="notfound" ]
-        then
+        if [[ ${found_in_two} == "notfound" ]];then
             one_own="${one_own}<${one}"
         fi
     done
     two_own="${arr_2[*]}"
     two_own=$(echo ${two_own// />})
     two_own=">${two_own}"
-    #echo "${one_own} ${one_cap_two} ${two_own}"
-    echo ""
+    echo "${one_own} ${one_cap_two} ${two_own}"
 }
 
-if [ ${1} = "show_diff" ]
-then
-    echo "local installed"
+extract_by_begin(){
+    status=(${1})
+    reg=${2}
+    for s in ${status[*]};do
+        if [[ ${s} == ${reg}* ]];then
+            own=$(echo ${s//${reg}/ })
+            #print_list "${own[*]}"
+        fi
+    done
+    echo "${own[*]}"
+}
+
+diff(){
+    #echo "local installed"
     local_installed=($(get_local_installed))
-    print_list "${local_installed[*]}"
-    echo ""
+    #print_list "${local_installed[*]}"
+    #echo ""
 
-    echo "remote supposed"
+    #echo "remote supposed"
     remote_supposed=($(get_remote_supposed))
-    print_list "${remote_supposed[*]}"
-    echo ""
+    #print_list "${remote_supposed[*]}"
+    #echo ""
 
-    echo "status"
+    #echo "status"
     status=$(compare "${local_installed[*]}" "${remote_supposed[*]}")
-    print_list "${status[*]}"
+    #print_list "${status[*]}"
+    echo "${status[*]}"
+}
+
+operation="${1}"
+if [[ ${operation} == "show_diff" ]];then
+    status=($(diff))
+    one_own=$(extract_by_begin "${status[*]}" "\<")
+    echo "one_own"
+    print_list "${one_own[*]}"
+    echo ""
+    both=$(extract_by_begin "${status[*]}" "\|")
+    echo "both"
+    print_list "${both[*]}"
+    two_own=$(extract_by_begin "${status[*]}" "\>")
+    echo "two_own"
+    print_list "${two_own[*]}"
 fi
+fi [[ ${operation} == "force_" ]]
 #if [ ${1} = "local" ]
 #then
 #    # 获取每一行然后运行
--- readme.md
+++ readme.md
@@ -1,20 +1,8 @@
 # readme
-同步方法，每次想更新配置，需要先从从远程拉取最新配置，然后更新到本地，然后手动更新本地配置，然后推送到远程中
-## 查看当前配置与文件的差别
-
-## 生成插件表
-### 生成本机插件表
-windows-git bash/linux-ternimal
-```shell
-bash generate_local_extension.sh
+## 更新插件方法
+linux下使用以下命令进行插件操作
+```bash
+bash extension_operation.sh **
 ```
-### 生成remote-ssh插件环境
-打开remote工程，在remote-ssh中运行generate_local_extension.sh脚本，运行方法与本机方法一致
-## 应用插件表
-### 本机应用插件表
-windows-git bash/linux ternimal
-```shell
-bash extensions_local_sync.sh
-```
-### remote环境应用插件表
-打开remote工程，在remote-ssh中运行extensions_remote_sync.sh，运行方法与本机方法一致
+* show_diff:bash extension_operation.sh show_diff
+显示出本地安装的插件与extensions.list的差异
